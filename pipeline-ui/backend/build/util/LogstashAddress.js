72e47f97dd924754324aad5aa2687b581b191f1c
Patrik Jonsson <patrik.jonsson@gmail.com>
2020-12-13 16:22:13 +0100

    Removing build folder

--- pipeline-ui/backend/build/util/LogstashAddress.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.LOGSTASH = void 0;
-exports.LOGSTASH = process.env.NODE_ENV === 'DEV' ? 'localhost' : 'logstash';
