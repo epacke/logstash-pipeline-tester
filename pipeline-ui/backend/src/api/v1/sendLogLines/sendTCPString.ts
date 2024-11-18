82e602f43e0a3e57ed35d6bf17c8b1c6ac98fbee
Patrik Jonsson <patrik.jonsson@gmail.com>
2024-11-18 22:03:55 +0100

    Adding more detailed error message when backend is unable to contact logstash

--- pipeline-ui/backend/src/api/v1/sendLogLines/sendTCPString.ts
+++ pipeline-ui/backend/src/api/v1/sendLogLines/sendTCPString.ts
@@ -4,11 +4,15 @@ import {LOGSTASH} from '../../../constants/LogstashAddress';
 import logger from '../../../util/Logger';
 
 function sendTCP(payload: string, port: number) {
-  const conn = net.createConnection({host: LOGSTASH, port: port}, function() {
+  const conn = net.createConnection({host: LOGSTASH, port: port}, function () {
     conn.write(payload);
-  }).on('error', function(err) {
-    logger.error(`Failed to send payload to ${port}. 
-    Has all logstash pipelines started successfully?`);
+  }).on('error', function (err) {
+    logger.error({
+      message: `Failed to send payload to ${port}.` +
+        `Has all logstash pipelines started successfully?`,
+      payload,
+      details: err.message,
+    });
   });
 }
 
