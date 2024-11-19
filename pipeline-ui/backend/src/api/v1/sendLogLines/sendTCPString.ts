6d470b3641f4e0cdbdce0016b8240d5b35b61657
Patrik Jonsson <patrik.jonsson@gmail.com>
2024-11-19 18:54:15 +0100

    Adding TCP send retries

--- pipeline-ui/backend/src/api/v1/sendLogLines/sendTCPString.ts
+++ pipeline-ui/backend/src/api/v1/sendLogLines/sendTCPString.ts
@@ -2,17 +2,30 @@
 import net from 'net';
 import {LOGSTASH} from '../../../constants/LogstashAddress';
 import logger from '../../../util/Logger';
+import config from '../../../constants/config';
 
-function sendTCP(payload: string, port: number) {
-  const conn = net.createConnection({host: LOGSTASH, port: port}, function () {
+function sendTCP(payload: string, port: number, retries = 0) {
+  const conn = net.createConnection({host: LOGSTASH, port: port}, function() {
     conn.write(payload);
-  }).on('error', function (err) {
+  }).on('error', function(err) {
     logger.error({
       message: `Failed to send payload to ${port}.` +
         `Has all logstash pipelines started successfully?`,
       payload,
       details: err.message,
     });
+    setTimeout(() => {
+      if (retries > config.maxRetries) {
+        logger.error({
+          message: `Gave up sending payload to ${port}. Timeout reached.`,
+          payload,
+          details: err.message,
+        });
+        return;
+      }
+      logger.info(`Retrying sending payload to port ${port}`);
+      sendTCP(payload, port, ++retries);
+    }, config.retryWaitTimeSeconds * 1000);
   });
 }
 
